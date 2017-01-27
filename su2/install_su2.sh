921560a536cef84a490e47806810dc1cd2454f49
Eivind Liland <spookysys@gmail.com>
2017-01-27 19:47:33 +0100

    Attempt to fix SU2 installation

--- su2/install_su2.sh
+++ su2/install_su2.sh
@@ -16,7 +16,6 @@ apt-get install -qqy --no-install-recommends \
     libopenmpi-dev \
     liblapack-dev liblapacke-dev libopenblas-dev \
     ssh
-# python-dev
 
 # Download and untar
 wget -qO- https://github.com/su2code/SU2/archive/v$VERSION.tar.gz | bsdtar -xvf- 
@@ -25,20 +24,21 @@ mv SU2* SU2
 # Create and activate environment for SU2
 conda create -qqy --name su2 python=2.7 numpy scipy mpi4py
 source activate su2
-
-exit
+echo "CONDA_PREFIX: $CONDA_PREFIX"
 
 # Configure and build SU2
 mkdir -p $SU2_HOME 
 cp -R SU2/* $SU2_HOME 
 cd /tmp/SU2 
-sed -ri -e 's|(PYTHON_INCLUDE\s=\s).*|\1-I$CONDA_PREFIX/include/python2.7|g' SU2_PY/pySU2/Makefile.* 
-sed -ri -e 's|(NUMPY_INCLUDE\s=\s).*|\1$CONDA_PREFIX/lib/python2.7/site-packages/numpy/core/include|g' SU2_PY/pySU2/Makefile.* 
-# TODO: MPI4PY_INCLUDE
-sed -ri -e 's|(TECIO_LIB\s=).*|\1|g' SU2_PY/pySU2/Makefile.* 
+sed -ri -e 's|(PYTHON_INCLUDE\s=\s)|\1-I${CONDA_PREFIX}/include/python2.7 |g' SU2_PY/pySU2/Makefile.*
+sed -ri -e 's|(NUMPY_INCLUDE\s=\s)|\1${CONDA_PREFIX}/lib/python2.7/site-packages/numpy/core/include |g' SU2_PY/pySU2/Makefile.*
+sed -ri -e 's|(MPI4PY_INCLUDE\s=\s)|\1${CONDA_PREFIX}/lib/python2.7/site-packages/mpi4py/include |g' SU2_PY/pySU2/Makefile.*
+sed -ri -e 's|(-lpython2.7)|\1 -L${CONDA_PREFIX}/lib|g' SU2_PY/pySU2/Makefile.*
+sed -ri -e 's|(TECIO_LIB\s=).*|\1|g' SU2_PY/pySU2/Makefile.*
 chmod ug+x configure preconfigure.py 
 ./configure \
     CXXFLAGS="-O2 -g" \
+    --srcdir=$SU2_HOME \
     --enable-mpi --with-cc=/usr/bin/mpicc --with-cxx=/usr/bin/mpicxx \
     --with-LAPACK-lib=/usr/lib --with-LAPACK-include=/usr/include \
     --enable-PY_WRAPPER \
